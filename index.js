82945d118fe1598219ff7a3f3a6ffd70a8171a90
Sean Hong <writeseanhong@gmail.com>
2018-08-17 16:20:45 -0500

    initial commit, transfered components and added javascript file

---
+++ index.js
@@ -0,0 +1,39 @@
+$(document).on("click", "#MenuItem", function() {
+  if (!$(".Submenu").hasClass("display")) {
+    var margin = 20;
+    var result = { X: 0, Y: 0 };
+    var topMargin = $("#MenuItemCover").width() / 2 - 25;
+    var leftMargin = $("#MenuItemCover").width() / 2 - 25;
+    var angle = 110;
+    $(".Submenu").each(function(k, v) {
+      $(this).addClass("display");
+      angle = angle + 30;
+      result.Y = parseInt(topMargin + 130 * Math.sin((angle * Math.PI) / 180));
+      result.X = parseInt(leftMargin + 130 * Math.cos((angle * Math.PI) / 180));
+      $(this).animate({
+        marginTop: result.Y,
+        marginLeft: result.X
+      });
+    });
+  } else {
+    $(".Submenu").each(function(k, v) {
+      $(this).removeClass("display");
+      $(this).animate({
+        marginTop: 75,
+        marginLeft: 75
+      });
+    });
+  }
+});
+
+$(document).ready(function() {
+  $("div.Submenu a").click(function(e) {
+    e.preventDefault();
+    $("html,body").animate(
+      {
+        scrollTop: $("#" + $(this).data("scroll")).offset().top
+      },
+      1000
+    );
+  });
+});
